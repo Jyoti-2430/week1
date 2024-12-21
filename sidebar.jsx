93132feea0d9ba8dc2a3e881ee71ad5a149edce3
Jyoti-2430 <jyoti.blr2402@gmail.com>
2024-12-21 15:25:40 +0530

    Add files via upload

---
+++ sidebar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+const sidebar=() =>{
+  return(
+    <div className="w-1/4 border border -s- white sidebar secondary_bg rounded-lg">S</div>
+  )
+}
+export default sidebar
